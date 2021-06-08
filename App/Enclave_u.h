9fd568a6ce3aa22d0bbaa40b9cc8ed24c6d231f4
ShuntaIshihara <ishihara@kde.cs.tsukuba.ac.jp>
2021-06-08 05:23:18 +0000

    check hash digest of sgx_sha256_msg

---
+++ App/Enclave_u.h
@@ -0,0 +1,30 @@
+#ifndef ENCLAVE_U_H__
+#define ENCLAVE_U_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include <string.h>
+#include "sgx_edger8r.h" /* for sgx_status_t etc. */
+
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef OCALL_PRINT_DEFINED__
+#define OCALL_PRINT_DEFINED__
+void SGX_UBRIDGE(SGX_NOCONVENTION, ocall_print, (int* str));
+#endif
+
+sgx_status_t ecall_test(sgx_enclave_id_t eid, int* retval, const char* message, size_t message_len);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
